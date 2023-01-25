81093bc60eb51f1114de25a2cea7a28684f67350
YasaswiniKoti <119840576+YASASWINIKOTI@users.noreply.github.com>
2023-01-25 19:48:56 +0530

    #include<stdio.h> int main() {     int n,m,e;     scanf("%d%d%d",&n,&m,&e);     if(n<=m*e)     {         printf("YES");         }         else         {             printf("NO");             }
    }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,e;
+    scanf("%d%d%d",&n,&m,&e); 
+    if(n<=m*e) 
+    {
+        printf("YES");
+        }
+        else
+        {
+            printf("NO");
+            }
+    
+}

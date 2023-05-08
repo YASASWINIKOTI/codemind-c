c7042b61f6e839b955716dae5c286a97a9f0f00c
YasaswiniKoti <119840576+YASASWINIKOTI@users.noreply.github.com>
2023-05-08 13:54:23 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d" ,&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j || j==(n+1)-i)             {                 printf("x" );                 }                 else { printf("0" );                 }                 }                 printf(" ");
        }}

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
-{ int n,i,j; 
-scanf("%d" ,&n); 
-for(i=1;i<=n;i++) 
 {
-    for(j=1;j<=i;j++)
+    int n,i,j;
+    scanf("%d" ,&n);
+    for(i=1;i<=n;i++)
     {
-        if(j==1 || i==n || i==j)
+        for(j=1;j<=n;j++)
         {
-            printf("*" ); 
-            
-        } 
-        else 
-        {
-            printf(" " );
-            }
-            }
-            printf("
-");}}
+            if(i==j || j==(n+1)-i) 
+            {
+                printf("x" );
+                }
+                else { printf("0" );
+                }
+                }
+                printf("
+");
+        
+    }}

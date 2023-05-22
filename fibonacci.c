72bf142c7ec283620eaf4d0894ef1b09e8f3a7e0
YasaswiniKoti <119840576+YASASWINIKOTI@users.noreply.github.com>
2023-05-22 18:46:34 +0530

    #include<stdio.h> int main() {     int a=0,b=1,i,c=0,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         printf("%d ",a);         c=a+b;         a=b;         b=c;         }         }

--- fibonacci.c
+++ fibonacci.c
@@ -3,13 +3,11 @@ int main()
 {
     int a=0,b=1,i,c=0,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=1;i<=n;i++) 
     {
         printf("%d ",a);
-        c=a+b;
+        c=a+b; 
         a=b;
         b=c;
-        
-    }
-    
-}
+        }
+        }
